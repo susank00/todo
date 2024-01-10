a1805b7e0e4aa2e644b2cf2609dab9d02142b19a
susankhatri00 <susankhatri00@gmail.com>
2024-01-10 11:12:48 +0545

    playing with use ref(uncontrolled element)

--- src/pages/add-todo/AddToDo.js
+++ src/pages/add-todo/AddToDo.js
@@ -1,8 +1,23 @@
+import { useRef } from "react";
 import NavBar from "../NavBar";
 const AddToDo = () => {
+  const todoText = useRef();
+  const addToDo = (e) => {
+    e.preventDefault();
+    console.log(todoText.current.value);
+  };
+
   return (
     <>
-      <NavBar /> this is the add page
+      <NavBar />
+      <div className="todo_container">
+        ADD to do
+        <br />
+        <form onSubmit={addToDo}>
+          <input type="text" ref={todoText} />
+          <button>save</button>
+        </form>
+      </div>
     </>
   );
 };
--- src/pages/mainPage/MainPage.js
+++ src/pages/mainPage/MainPage.js
@@ -1,8 +1,5 @@
-import { Link, useHistory } from "react-router-dom";
 import NavBar from "../NavBar";
 const MainPage = () => {
-  const history = useHistory();
-
   return (
     <>
       <NavBar />
