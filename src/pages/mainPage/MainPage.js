666b5a5d8e92687c6df78775e45a89ce545ebb95
susankhatri00 <susankhatri00@gmail.com>
2024-01-11 09:57:12 +0545

    writing go back function using useHistoryin viewpage.js

--- src/pages/mainPage/MainPage.js
+++ src/pages/mainPage/MainPage.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import NavBar from "../NavBar";
 import { FaRegEye } from "react-icons/fa";
 
@@ -11,11 +12,18 @@ const MainPage = () => {
 
       <div className="todo_container">
         <h3>your todo:</h3>
-        {getTodo.map((el) => (
+        {getTodo.map((el, index) => (
           <>
             <div className="single_todo">
               {el}
-              <FaRegEye size={"20px"} />
+              <Link to={`/view/${index}`}>
+                <FaRegEye size={"20px"} />{" "}
+              </Link>
+              {/*
+              <Link to={"/view/" + el}>
+                <FaRegEye size={"20px"} />{" "}
+              </Link>{" "}      this i salso used but old we will use back tick easy
+        */}
             </div>
           </>
         ))}
--- src/pages/mainPage/ViewPage/ViewPage.js
+++ src/pages/mainPage/ViewPage/ViewPage.js
@@ -1,4 +1,17 @@
+import { useHistory } from "react-router-dom";
+import NavBar from "../../NavBar";
+
 const ViewPage = () => {
-  return <> View page</>;
+  const history = useHistory();
+  const goBack = () => {
+    history.push("/");
+  };
+
+  return (
+    <>
+      <NavBar />
+      <button onClick={goBack}>Go back</button>
+    </>
+  );
 };
 export default ViewPage;
