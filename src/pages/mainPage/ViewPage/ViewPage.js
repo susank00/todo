8fec4a9c08ec6c4717a8acd0fdeeda0222abdd38
susankhatri00 <susankhatri00@gmail.com>
2024-01-11 10:28:42 +0545

    dynamic routing using param

--- src/pages/mainPage/ViewPage/ViewPage.js
+++ src/pages/mainPage/ViewPage/ViewPage.js
@@ -1,8 +1,14 @@
-import { useHistory } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
 import NavBar from "../../NavBar";
 
 const ViewPage = () => {
   const history = useHistory();
+  const getParams = useParams();
+  const getID = getParams.id;
+  const getStorage = localStorage.getItem("todo")
+    ? JSON.parse(localStorage.getItem("todo"))
+    : [];
+  const getData = getStorage[getID];
   // const goBack = () => {
   //(do this when there is long code or huge project) history.push("/"); };
 
@@ -10,14 +16,24 @@ const ViewPage = () => {
     <>
       <NavBar />
       {/*<button onClick={goBack}>Go back</button>   this is another way*/}
-
-      <button
-        onClick={() => {
-          history.push("/");
-        }}
-      >
-        Go back
-      </button>
+      <div clasName="todo_container">
+        <button
+          onClick={() => {
+            history.push("/");
+          }}
+        >
+          Go back
+        </button>
+        <div
+          style={{
+            background: "red",
+            padding: "10px",
+            maxWidth: "100px",
+          }}
+        >
+          {getData}
+        </div>
+      </div>
     </>
   );
 };
