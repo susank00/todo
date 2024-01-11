b22974010f1555315019cea48ec9e88633783231
susankhatri00 <susankhatri00@gmail.com>
2024-01-11 10:06:01 +0545

    adding anonymous function in the view.js page

--- src/pages/mainPage/ViewPage/ViewPage.js
+++ src/pages/mainPage/ViewPage/ViewPage.js
@@ -3,14 +3,21 @@ import NavBar from "../../NavBar";
 
 const ViewPage = () => {
   const history = useHistory();
-  const goBack = () => {
-    history.push("/");
-  };
+  // const goBack = () => {
+  //(do this when there is long code or huge project) history.push("/"); };
 
   return (
     <>
       <NavBar />
-      <button onClick={goBack}>Go back</button>
+      {/*<button onClick={goBack}>Go back</button>   this is another way*/}
+
+      <button
+        onClick={() => {
+          history.push("/");
+        }}
+      >
+        Go back
+      </button>
     </>
   );
 };
